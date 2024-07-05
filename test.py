2144f995f983c0511066984483892c73f156a13d
Jan Mikloško <jan.miklosko@outlook.com>
2024-07-05 21:22:07 +0200

    test

--- test.py
+++
@@ -1,7 +0,0 @@
-def rain_amount(mm):
-    if mm >= 40:
-        return "Your plant has had more than enough water for today!"
-    elif mm < 40:
-        return f"You need to give your plant  {40 - mm}mm of water" 
-    
-print(rain_amount(39))
